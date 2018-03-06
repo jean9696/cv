b222344dfbe8cdf1dd2657cd5ddc0d074432f054
Jean Dessane <jean.dessane@gmail.com>
2018-03-06 10:36:43 +0000

    social

--- src/LandingPage/Description/Description.js
+++ src/LandingPage/Description/Description.js
@@ -5,6 +5,10 @@ import { connect } from 'react-redux';
 import { select } from '../../redux/reducers';
 import styles from './description.scss';
 import lang from './description.lang';
+import twitter from './socialIcons/twitter.svg';
+import github from './socialIcons/github.svg';
+import linkedin from './socialIcons/linkedin.svg';
+import facebook from './socialIcons/facebook.svg';
 
 const handleShowCv = () => window.open('https://www.dropbox.com/s/ydwsreqq1eckp7f/Resume%202018.jpg?dl=0');
 
@@ -16,6 +20,20 @@ const Description = props => (
     <p className={styles.desc}>
       {props.trad(lang.description)}
     </p>
+    <div className={styles.socials}>
+      <a href="https://github.com/jean9696" target="_blank" rel="noopener noreferrer">
+        <img src={github} alt="github" />
+      </a>
+      <a href="https://www.linkedin.com/in/jeandessane/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedin} alt="linkedin" />
+      </a>
+      <a href="https://www.facebook.com/jean.dessane" target="_blank" rel="noopener noreferrer">
+        <img src={facebook} alt="facebook" />
+      </a>
+      <a href="https://twitter.com/JDessane" target="_blank" rel="noopener noreferrer">
+        <img src={twitter} alt="twitter" />
+      </a>
+    </div>
     <Divider />
     <div className={styles.buttonContainer}>
       <Button variant="raised" color="primary" className={styles.button} onClick={handleShowCv}>
